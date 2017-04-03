138a3175422f4194875e0f8a6e6391f832c57e7c
Saul Greene <spdysg@gmail.com>
2017-04-03 16:22:23 -0700

    Health bar working, takes an argument for damage

--- public/scripts/views/battleView.js
+++ public/scripts/views/battleView.js
@@ -1,1 +1,41 @@
 'use strict';
+
+
+var hitBtn = $('button.damage'),
+  healthBar = $('.health-bar'),
+  bar = healthBar.find('.bar'),
+  hit = healthBar.find('.hit');
+
+healthBar.data('value', healthBar.data('total'));
+hit.css({'width': '0'});
+bar.css('width', '100%');
+bar.css({'background': '#7FFF00'})
+
+// hitBtn.on("click", function(){// this will be changed to an invokable function once done with testing....
+// var damage = PLACEHOLER FOR THE HP FROM THE API
+var attackExecute = function(damage){
+  var total = healthBar.data('total'),
+    value = healthBar.data('value');
+  //Calculation of damage dealt and total width of the health bar
+  //change this to a PLACEHOLDER VAR
+  var newValue = value - damage;
+  var barWidth = (newValue / total) * 100;
+  var hitWidth = (damage / value) * 100 + "%";
+
+  // show hit bar and set the width
+  hit.css('width', hitWidth);
+  healthBar.data('value', newValue);
+
+  setTimeout(function(){
+    hit.css({'width': '0'});
+    bar.css('width', barWidth + "%");
+  }, 500);
+
+//this chages the color of the damage bar
+  if( newValue < 300){
+    bar.css({'background': 'red'})
+  } else if(newValue > 300){
+    bar.css({'background': '#7FFF00'})
+  }
+  console.log("Remaining Health:"+ newValue +" Damage:"+ damage);
+};
