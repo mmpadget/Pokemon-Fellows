af6a3d8d0663823d27ae0404f7ecb020c887f25c
W. Ely Paysinger <paysinger@gmail.com>
2017-04-04 08:44:41 -0700

    battle.js encapsulated, vars hooked to pokemon object

--- public/scripts/models/battle.js
+++ public/scripts/models/battle.js
@@ -1,25 +1,7 @@
 'use strict';
 
 $(function(module){
-
-  // socket.io variables
-  const socket = io();
-  socket.arena;
-  socket.host = false;
-  socket.haveSecondPlayer = false;
-  // end socket.io vars
-
-  let urlNumbers = []; //do some math and get 3 random numbers
-  let pokes = []; // array of pokes
-  let pokeObjects = [];
-  let thierPokes = []; //array for their pokes
-  const pokeUrl = 'http://pokeapi.co/api/v2/pokemon/';
-
-  const PlayerConstructor = function(pokes, arena){
-    //your constructor here
-  };
-
-  const Poke = function(poke){
+  function Pokemon(poke){
     //pokeConstructor here
     // name, hp, moves, power
     this.name = poke.poke.name;
@@ -27,13 +9,30 @@ $(function(module){
     this.moveSet = poke.moveSet.map(function (move) {
       return {name: move.name, power: move.power};
     });
-    pokes.push(this);
-    console.log(pokes);
-  };
+    Pokemon.pokes.push(this);
+    console.log(Pokemon.pokes);
+  }
 
-  function getSomePokes(){
-    urlNumbers.forEach(function(number){
-      $.get(`${pokeUrl}${number}/`, function(response){
+  // socket.io variables
+  const socket = io(); //acceptable linter error, io is defined in the js file added by the socket.io node module.
+  socket.arena;
+  socket.host = false;
+  socket.haveSecondPlayer = false;
+  // end socket.io vars
+
+  Pokemon.urlNumbers = []; //do some math and get 3 random numbers
+  Pokemon.pokes = []; // array of pokes
+  Pokemon.pokeObjects = [];
+  Pokemon.theirPokes = []; //array for their pokes
+  Pokemon.pokeUrl = 'http://pokeapi.co/api/v2/pokemon/';
+
+  // const Player = function(){
+  //   //your constructor here
+  // };
+
+  Pokemon.getSomePokes = () => {
+    Pokemon.urlNumbers.forEach(function(number){
+      $.get(`${Pokemon.pokeUrl}${number}/`, function(response){
         // 4 pokes. Array 0-3 // This is the HP response.stats[5].base_stat
         let poke = {poke: response, moveSet: []};
 
@@ -44,44 +43,56 @@ $(function(module){
             // See constructor: move.name is move name and move.power is move power. Move is response.
             poke.moveSet.push(move);
             if (poke.moveSet.length === 4) {
-              new Poke(poke);
+              new Pokemon(poke);
             }
           });
         }
       })
-      if (pokes.length === 3){
-        battleView.attackExecute(pokes);
+      if (Pokemon.pokes.length === 3){
+        battleView.attackExecute(Pokemon.pokes);
         //chain what happens next.
-        sendToOpponent();
+        Pokemon.sendToOpponent();
         // populate to page (pokes);
         // push to pokes;
         // if 3 pokes, sendtootherplayer()
       }
     });
-  }
+  };
 
   socket.on('connectToArena', function(data){
     socket.arena = data.arena;
     console.log(data.message);
+    socket.emit('player', {arena: socket.arena, player: true}); // on connect to server, broadcast I'm a player to arena.
   });
 
-  function populateToPage(){
+  socket.on('host', function(data){
+    socket.host = data.host;
+    console.log(data.message);
+  });
+
+
+  socket.on('player', function(data){
+    socket.arena = data.arena;
+    console.log(data.message);
+  });
+
+  Pokemon.populateToPage = () => {
     //put them onto page.
   }
 
-  function sendToOpponent(){
+  Pokemon.sendToOpponent = () => {
     //send them pokes with socket.io
   }
 
   // socket.on('sendPokes', function(pokes){ // pokes must be object with arena value.
     //populate their pokes to page. reuse populateToPage if possible.
   // });
-  function getRandomNumbers(){
+  Pokemon.getRandomNumbers = () => {
     //need urlNumbers to have 3 random number from 1-upper limit of api poke index
-    urlNumbers = [1,4,7];
+    Pokemon.urlNumbers = [1,4,7];
   }
 
-  getRandomNumbers();
-  // getSomePokes();
-  module.socket = socket;
+  Pokemon.getRandomNumbers();
+  Pokemon.getSomePokes();
+  module.Pokemon = Pokemon;
 }(window));
