d7759a26f6df5b892784cf4f8280798de2e0b510
Michael Padget <mpadget@comcast.net>
2017-04-03 18:59:25 -0700

    Working constructor, needs refactoring into two parts

--- public/scripts/models/battle.js
+++ public/scripts/models/battle.js
@@ -3,36 +3,57 @@
 // $(function(module){
   //runs on page load
   let arena;
-  let urlNumbers = []//do some math and get 3 random numbers
-  let pokes = []// array of pokes
-  let thierPokes = []//array for their pokes
-  const pokeUrl = 'http://pokeapi.co/api/v2/pokemon/'
+  let urlNumbers = []; //do some math and get 3 random numbers
+  let pokes = []; // array of pokes
+  let pokeObjects = [];
+  let thierPokes = []; //array for their pokes
+  const pokeUrl = 'http://pokeapi.co/api/v2/pokemon/';
 
   const PlayerConstructor = function(pokes, arena){
     //your constructor here
-    const PokeConst = function(pokes){
-      //pokeConstructor here
-    };
   };
 
+  const Poke = function(poke){
+    //pokeConstructor here
+    // name, hp, moves, power
+    // this.name = name...
+    // this.hp = hp...
+    // console.log(poke,'what is here???');
+    this.moves = poke.moves.map(function (move) {
+      return {name: move.name, power: move.power};
+    });
+    pokeObjects.push(this);
+    console.log(pokeObjects);
+  };
 
+  // First hold in local storage.
+
+  // Then build constructor.
 
   function getSomePokes(pokes, urlNumbers){
     urlNumbers.forEach(function(number){
       $.get(`${pokeUrl}${number}/`, function(response){
         // 4 pokes. Array 0-3 // This is the HP response.stats[5].base_stat
         let poke = {poke: response, moves: []};
-        for (var i = 0; i < 4; i++) {
+
+        for (let i = 0; i < 4; i++) {
           $.get(response.moves[i].move.url, function(move) {
             // have a url, assign API response: name, power, base_stat
             // console.log(move);
             // See constructor: move.name is move name and move.power is move power. Move is response.
+
             poke.moves.push(move);
+            // console.log(poke);
+            console.log('i: ', i);
+            if (i === 3) {
+              new Poke(poke);
+            }
           });
         }
+
         //build the constructor
-        pokes.push(poke);
-      });
+        //pokes.push(poke);;
+      })
       //chain what happens next.
       // populate to page (pokes);
       // push to pokes;
