0143ac512413aa34e79ec3ba1fc8905a623fb962
W. Ely Paysinger <paysinger@gmail.com>
2017-04-05 16:44:58 -0700

    page.js fixed

--- server.js
+++ server.js
@@ -25,68 +25,55 @@ app.use(express.static('./public'));
 
 // ------ GET ------
 
-// app.get('*', (req, res) => {
-//   res.sendFile('index.html', { root: './public' })
-// })
-
 app.get('/', function(req, res){
   res.sendFile('index.html', {root: './public'});
 });
 
-// app.get('/battle', function(req, res){
-//   startSocketConnection();
-// });
-
-// app.get('/about', function(req, res){
-//   res.sendFile('index.html', {root: './public'});
-// });
-
 // Socket.io information channels---------
-// function startSocketConnection(){
-  io.on('connection', function(socket){ //io.connection sets up the paths/connections
-    let arena = 'arena-' + arenaNum;
-    socket.join(arena, function(){
-      //Send this event to everyone in the room.
-      console.log('Player Conneted in ' + arena);
-      io.to(arena).emit('connectToArena', {
-        message: 'You are in ' + arena,
-        arena: arena
-      });
-      if (io.nsps['/'].adapter.rooms[arena].length === 1){
-        io.to(arena).emit('host', {
-          message: 'You are hosting this game',
-          host: true
-        })
-      }
+io.on('connection', function(socket){ //io.connection sets up the paths/connections
+  let arena = 'arena-' + arenaNum;
+  socket.join(arena, function(){
+    //Send this event to everyone in the room.
+    console.log('Player Conneted in ' + arena);
+    io.to(arena).emit('connectToArena', {
+      message: 'You are in ' + arena,
+      arena: arena
     });
-
-    socket.on('player', function(data){ // this sets up a listener route waiting for a client to send something on the 'player' route.
-      socket.broadcast.to(data.arena).emit('player', data.player); // broadcast the boolean true/data.value if the listener hears another client join.
-    });
-
-    socket.on('pokes', function(data){
-      socket.broadcast.to(data.arena).emit('pokes', data.pokes);
-    });
-
-    socket.on('attack', function(data){ //this is the route set up to handle attacks/switches
-      socket.broadcast.to(data.arena).emit('attack', data.attack);
-    });
-
-    socket.on('results', function(data){ //this is the route set up to handle attacks/switches
-      socket.broadcast.to(data.arena).emit('results', data.results);
-    });
-
-    socket.on('disconnect', function(socket){
-      console.log('a user disconnected', socket)
-      io.to(arena).emit('testDisconnect', true);
-    });
-
-    console.log('a user connected'); // this logs to the server console/terminal
-    if(io.nsps['/'].adapter.rooms[arena] && io.nsps['/'].adapter.rooms[arena].length > 1){
-      arenaNum++;
+    if (io.nsps['/'].adapter.rooms[arena].length === 1){
+      io.to(arena).emit('host', {
+        message: 'You are hosting this game',
+        host: true
+      })
     }
   });
-// }
+
+  socket.on('player', function(data){ // this sets up a listener route waiting for a client to send something on the 'player' route.
+    socket.broadcast.to(data.arena).emit('player', data.player); // broadcast the boolean true/data.value if the listener hears another client join.
+  });
+
+  socket.on('pokes', function(data){
+    socket.broadcast.to(data.arena).emit('pokes', data.pokes);
+  });
+
+  socket.on('attack', function(data){ //this is the route set up to handle attacks/switches
+    socket.broadcast.to(data.arena).emit('attack', data.attack);
+  });
+
+  socket.on('results', function(data){ //this is the route set up to handle attacks/switches
+    socket.broadcast.to(data.arena).emit('results', data.results);
+  });
+
+  socket.on('disconnect', function(socket){
+    console.log('a user disconnected', socket)
+    io.to(arena).emit('testDisconnect', true);
+  });
+
+  console.log('a user connected'); // this logs to the server console/terminal
+  if(io.nsps['/'].adapter.rooms[arena] && io.nsps['/'].adapter.rooms[arena].length > 1){
+    arenaNum++;
+  }
+});
+
 // end Socket.io channels------------------
 
 http.listen(PORT, function(){
